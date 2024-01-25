a61abf197485f1104fcf39a01513314a7b613bc8
Sanjay Shitole <scshitole@gmail.com>
2024-01-24 18:31:41 -0800

    update

--- main.tf
+++ main.tf
@@ -16,3 +16,16 @@ provider "bigip" {
   username = var.username
   password = var.password
 }
+
+resource "bigip_fast_application" "MyChart" {
+
+  template        = "epic_multi_env_templates/Epic Epic MyCHART"
+  fast_json = <<EOF
+{
+      "tenant_name": "EPIC_VS_MyChart",
+      "virtualAddress": "100.0.0.201",
+      "server_addresses": ["100.1.1.20","100.1.1.21"],
+      "service_port": "80"
+}
+EOF
+}
